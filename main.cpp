2c218cb452c9aa1431abb1be8c210b6a2c282230
Aatreya Kapoor <aatreya21@gmail.com>
2026-08-13 00:43:55 +0530

    created shell skeleton

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int shell_execute(std::vector<std::string> &args)
+{
+    return EXIT_SUCCESS;
+}
+
+void shell_parse(std ::string &line, std::vector<std::string> &args)
+{
+}
+
+void shell_loop(void)
+{
+    std::string line;
+    std::vector<std::string> args;
+    int status;
+
+    do
+    {
+        std::cout << "> ";
+        std::getline(std::cin, line);
+        shell_parse(line, args);
+        int status = shell_execute(args);
+
+        line.clear();
+        args.clear();
+    } while (status);
+}
+
+int main()
+{
+
+    shell_loop();
+
+    return EXIT_SUCCESS;
+}
