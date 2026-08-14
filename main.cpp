834469574a961fc8a045fe18d463ae9ca5f3f4cd
Aatreya Kapoor <aatreya21@gmail.com>
2026-08-15 01:39:26 +0530

    added delimiters and fixed status

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 #include <sstream>
 #include <cstdlib>
 
-int shell_execute(std::vector<std::string> &args)
+#define DELIMITERS " \t\r\n\a"
+
+int shell_execute(std::vector<std::string> &args) // TODO: Use windows CreateProcess and CreateThread to implement process forking + exec.
 {
     return EXIT_SUCCESS;
 }
@@ -13,7 +15,7 @@ void shell_parse(const std::string &line, std::vector<std::string> &args)
 {
     std::stringstream line_stream(line);
     std::string arg;
-    while(std::getline(line_stream, arg, ' ')) {
+    while(std::getline(line_stream, arg, DELIMITERS[0])) { // TODO: Add support for quoted arguments, and other delimiters later
         if(!arg.empty()) {
             args.push_back(arg);
         }
@@ -29,13 +31,14 @@ void shell_loop(void)
     do
     {
         std::cout << "> ";
-        std::getline(std::cin, line);
+        std::getline(std::cin, line); // TODO: Add support for multi-line and file commands later, shell_read_line() function will be created for that
         shell_parse(line, args);
         status = shell_execute(args);
 
         line.clear();
         args.clear();
-    } while (status);
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    } while (!status);
 }
 
 int main()
