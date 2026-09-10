bfe1144b520e72ac07e827eb898307a5be804586
Aatreya Kapoor <aatreya21@gmail.com>
2026-09-11 02:55:44 +0530

    implemented read using ReadConsoleInput to get control over keystrokes in real time

--- src/shell_read.cpp
+++ src/shell_read.cpp
@@ -5,40 +5,62 @@
 #include <fstream>
 #include <cstdlib>
 #include <limits>
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
 #include <windows.h>
 #include <algorithm>
 #include "shell_cmds.hpp"
 #include "shell_read.hpp"
 #include "utils.hpp"
 
+constexpr int TAB_SIZE = 4;
+
 void handle_autocomplete(std::string &line) {
     if(line == "exi") {
         prefill_input("exit");
-        // std::cout<<"exit"<<std::endl;
     } else if (line == "ech") {
         prefill_input("echo");
-        // std::cout<<"echo"<<std::endl;
     }
 }
 
+
+
 void shell_read(std::string &line, int &status) {
-    char ch;
-    while(std::cin.get(ch) && (ch != '\n')) {
-        if(ch == '\t') {
-            status = SHELL_STATUS_NORM; // TODO: turn this to an extra command here
-            handle_autocomplete(line);
-            std::cin.ignore(
-                std::numeric_limits<std::streamsize>::max(),
-                '\n'
-            );
-            std::string var = "autocomp " + line;
-            line = var;
-            return;
-        } else {
-            line += ch;
+    HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    DWORD prevHandle;
+    GetConsoleMode(hIn, &prevHandle);
+
+    SetConsoleMode(hIn, prevHandle & ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT));
+
+    std::string inputBuffer;
+    INPUT_RECORD ir;
+    DWORD read;
+
+    while(true) {
+        ReadConsoleInput(hIn, &ir, 1, &read);
+
+        if(ir.EventType == KEY_EVENT && ir.Event.KeyEvent.bKeyDown) {
+            auto &ke = ir.Event.KeyEvent;
+            WORD vk = ke.wVirtualKeyCode;
+            char ch = ke.uChar.AsciiChar;
+
+            if(vk == VK_ESCAPE) break;
+
+            if(vk == VK_BACK) {
+                if(!inputBuffer.empty()) {
+                    inputBuffer.pop_back();
+                    std::cout<< "\b \b" << std::flush;
+                }
+            } else if (vk == VK_RETURN) {
+                line = inputBuffer;
+                std::cout << std::endl;
+                inputBuffer.clear();
+                break;
+            } else if (ch >= 32 && ch <= 126) {
+                inputBuffer.push_back(ch);
+                std::cout<<ch<<std::flush;
+            }
         }
     }
+    SetConsoleMode(hIn, prevHandle);
 }
